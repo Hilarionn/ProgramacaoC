cb71fff560661005e2542f1cda9afaa04f4d5073
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-10-16 23:34:11 +0000

    Começo

--- horadecodar_mestre.c
+++ horadecodar_mestre.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
-int main (){
+int main () {
 //Declarar variáveis Produto, U i estoque, double valor unitário, double valor total e 
   // u i quantidade mínima
+
+  
+char produtoA[30] = "Produto A";
+char produtob [30] = "Produto B";
+
+unsigned int estoqueA = 1000;
+unsigned int estoqueB = 2000;
+
+float valorA = 10.50;
+float valorB = 20.40;
+
+unsigned int estoqueMinimoA = 500;
+unsigned int estoqueMinimoB = 2500;
+
+
+double valorTotalA;
+double ValorTotalB;
+
 
 
   //exibir as informações dos produtos
