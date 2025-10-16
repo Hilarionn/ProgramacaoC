a0a566999a711a181f50c568f341e264db7302cf
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2025-10-16 20:27:32 -0300

    Create horadecodar_mestre.c with main structure
    Initial commit of horadecodar_mestre.c with structure for product management.

---
+++ horadecodar_mestre.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+//Declarar variáveis Produto, U i estoque, double valor unitário, double valor total e 
+  // u i quantidade mínima
+
+
+  //exibir as informações dos produtos
+
+  //comparações com o valor mínimo de estoque
+
+  //Comparações entre os valores totais dos produtos
+
+
+
+
+
+
+return 0;
+}
