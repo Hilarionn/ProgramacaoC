37d76eaaffb850aeda7d312b9d93f801675a05b0
Gabriel Hilarion <email>
2025-10-20 19:30:09 -0300

    teste

---
+++ teste.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main () {
+
+int sucupira;
+int sacapara;
+
+sucupira = 10;
+sacapara = 20;
+
+if (sucupira > sacapara)
+{
+    printf("SUCUPIRA is greater than SACAPARA\n");
+}
+else {
+    printf("SACAPARA is greater than SUCUPIRA\n");
+
+    return 0;
+
+}
