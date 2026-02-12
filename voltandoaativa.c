9117770849006fe722cb9226043fa172f564a239
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-12 23:23:38 +0000

    simm

--- voltandoaativa.c
+++ voltandoaativa.c
@@ -107,7 +107,7 @@ scanf("%d", &idade);
     if(idade < 12){
         printf("Você é uma criança.\n");
     } else if (idade >= 12 && idade < 18) {printf("Você é um adolescente!\n");}
-        else if (idade >== 18 && idade < 60) { printf("Você é um adulto!\n");}
+        else if (idade >= 18 && idade < 60) { printf("Você é um adulto!\n");}
         else if {printf("Você é um idoso!");}
     return 0;
 }
