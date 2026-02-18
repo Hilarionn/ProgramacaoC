aa69cf101a05b30dac0416dc770bc64ed7ffe55a
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-18 11:09:53 +0000

    testee

--- Desafio1.c
+++ Desafio1.c
@@ -117,32 +117,32 @@ printf("1. População\n2. PIB\n3. Área em KM²\n4. Pontos turísticos\n5. Dens
     switch (atributo) {
     
     case 1: 
-        if (populacao_1 > populacao_2) { printf("A população 1 é maior que a população 2!\n"); }
-        else if (populacao_1 < populacao_2) {printf("A populacao_2 é maior que a população 1!\n");}
+        if (populacao_1 > populacao_2) { printf("A população 1 (%d) é maior que a população 2!\n", populacao_1); }
+        else if (populacao_1 < populacao_2) {printf("A populacao 2 (%d) é maior que a população 1!\n", populacao_2);}
         else {printf("As cartas possuem o mesmo número em população!\n");}
     break;
     
     case 2:
-        if (pib_1 > pib_2) { printf("O PIB 1 é maior que o PIB 2!\n"); }
-        else if (pib_1 < pib_2) {printf("O PIB 1 é menor que o PIB 2!\n");}
-        else {printf("As cartas possuem o mesmo número em PIB!\n");}
+        if (pib_1 > pib_2) { printf("O PIB 1 R$(%.2f) é maior que o PIB 2 (R$%.2f)!\n", pib_1, pib_2);}
+        else if (pib_1 < pib_2) {printf("O PIB 1 R$(%.2f) é menor que o PIB 2 (R$%.2f)!\n", pib_1, pib_2);}
+        else {printf("As cartas possuem o mesmo número em PIB! (R$%.2f)\n"), pib_1;}
     break;
     
     case 3: 
-        if (area_km2_1 > area_km2_2) { printf("A área 1 é maior que a área 2!\n"); }
-        else if (area_km2_1 < area_km2_2) {printf("A área 1 é menor que a área 2!\n");}
+        if (area_km2_1 > area_km2_2) { printf("A área 1 (%f) é maior que a área 2(R$%.2f)!\n", area_km2_1, area_km2_2); }
+        else if (area_km2_1 < area_km2_2) {printf("A área 1 (%f) é menor que a área 2 (%f)!\n", area_km2_1, area_km2_2);}
         else {printf("A área das duas cartas é igual!\n");}
     break;
     
     case 4:
-        if (pontos_turisticos_1 > pontos_turisticos_2) { printf("A primeira carta tem mais pontos turísticos!\n"); }
-        else if (pontos_turisticos_1 < pontos_turisticos_2) {printf("A segunda carta tem mais pontos turísticos!\n");}
+        if (pontos_turisticos_1 > pontos_turisticos_2) { printf("A primeira carta tem mais pontos turísticos (%d)!\n", pontos_turisticos_1); }
+        else if (pontos_turisticos_1 < pontos_turisticos_2) {printf("A segunda carta tem mais pontos turísticos (%d)!\n", pontos_turisticos_2);}
         else {printf("As cartas tem o mesmo número de pontos turísticos!\n");}
     break;
     
     case 5: 
-        if (densidade_pop1 < densidade_pop2) {printf("A densidade populacional da primeira carta é menor do que a da segunda, logo, primeira carta vence!");}
-        else if (densidade_pop1 > densidade_pop2) {printf("A densidade populacional da segunda carta é menor do que a da primeira, logo, segunda carta vence!");}
+        if (densidade_pop1 < densidade_pop2) {printf("A densidade populacional da primeira carta (%.2f) é menor do que a da segunda (%.2f), logo, primeira carta vence!", densidade_pop1, densidade_pop2);}
+        else if (densidade_pop1 > densidade_pop2) {printf("A densidade populacional da segunda carta (%.2f) é menor do que a da primeira (%.2f), logo, segunda carta vence!", densidade_pop1, densidade_pop2);}
         else {printf("Opção inválida\n");}
     break;
     
