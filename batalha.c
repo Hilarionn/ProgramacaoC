7ee3cded8252fbab7f4ebbc9cb34a3e13f99cd70
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-18 11:32:41 +0000

    joguin

---
+++ batalha.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main () {
+//Definindo cartas
+
+//=====LOBO=====//
+char lobo;
+int ataque_lobo;
+int defesa_lobo;
+int velocidade_lobo;
+int vida_lobo;
+int dano_critico_lobo;
+//=====LEÃO=====//
+
+
+
+
+
+//=====TOURO=====//
+
+// Ataque vai subtrair a velocidade dividida por 3, o resultado vai subtrair totalmente da defesa, a cada nova rodada, o ponto de defesa será reduzido no resultado
+//da subtração ATAQUE - DEFESA / 3, o dano crítico remove HP desconsiderando defesa. Vai ser por probabilidade.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
