8a23454d1ec077633425b4b78408f5c90a527074
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-19 23:00:39 +0000

    testes

--- while.c
+++ while.c
@@ -6,9 +6,13 @@ int main () {
    while (i <= 5) {
        
        printf("%d\n", i);
-       i++; //Se comentar essa linha, vai imprimir a variável até o programa travar.
+       i = i + 4 ; //Se comentar essa linha, vai imprimir a variável até o programa travar.
        //Pois a variável nunca vai ser maior que 1.
+
+
+       //Você pode fazer assim: i = i + 4; ou i = i + g; (Tendo definido valor pra uma variável chamada G).
    }
     
-    
+    return 0;
+}
     
