518e3f75a2b53b2c6f593a4bd0d7f673a1160600
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-19 23:42:23 +0000

    message

--- dowhile.c
+++ dowhile.c
@@ -2,14 +2,38 @@
 int main() {
     int i = 1;
 
+    if (i >= 9999) { printf("Você ganhou!");}
+    else {
+
+    
     do {
         printf("%d\n", i);
        // i++;
-       i = i + 15;
-    } while (i <=5);
-
+       i = i * 6;
+    } while (i <=9999);
+    }
 /* Primeiro ele printa, depois ele verifica o valor da variável. A vantagem desse modelo é que ele imprime a informação ao menos uma vez
  Independente de se a condição for verdadeira ou falsa.*/
 
 return 0;
 }
+
+
+/*
+
+#include <stdio.h>
+
+int main() {
+
+    int i = 1;
+
+    do {
+
+        printf("%d\n", i);
+        i++;
+
+    } while (i == 5);
+
+    return 0;
+}
+*/
