c420c07bff11287e2638cf4ad96c0b238c2fa312
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-19 23:07:14 +0000

    while

--- while.c
+++ while.c
@@ -3,11 +3,11 @@ int main () {
     
    int i = 1;
    
-   while (i <= 5) {
+   while (i <= 5) { //Enquanto i é menor ou igual a 5, executar o printf
        
-       printf("%d\n", i);
-       i = i + 4 ; //Se comentar essa linha, vai imprimir a variável até o programa travar.
-       //Pois a variável nunca vai ser maior que 1.
+       printf("%d\n", i); // Comando executado pela linha anterior
+       i++; //Comando a ser executado logo em sequência do printf. Se comentar essa linha, vai imprimir a variável até o programa travar. 
+       //Pois o incremento nunca acontece e a variável nunca vai ser maior que 1.
 
 
        //Você pode fazer assim: i = i + 4; ou i = i + g; (Tendo definido valor pra uma variável chamada G).
@@ -15,4 +15,3 @@ int main () {
     
     return 0;
 }
-    
