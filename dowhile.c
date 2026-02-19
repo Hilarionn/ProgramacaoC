517172ede3f158d0dfcdaf1b5ac9ef0e7f3a8ac3
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-19 23:18:06 +0000

    dowhile

--- dowhile.c
+++ dowhile.c
@@ -4,10 +4,12 @@ int main() {
 
     do {
         printf("%d\n", i);
-        i++;
+       // i++;
+       i = i + 15;
     } while (i <=5);
 
-// Primeiro ele printa, depois ele verifica o valor da variável.
+/* Primeiro ele printa, depois ele verifica o valor da variável. A vantagem desse modelo é que ele imprime a informação ao menos uma vez
+ Independente de se a condição for verdadeira ou falsa.*/
 
 return 0;
 }
