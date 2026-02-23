d4e5978b007ee2dd0ec3eb3e5e1659e177916b09
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-23 13:02:57 +0000

    rainha

--- cadastro-cartas-Hilarionn-main/desafioxadrez.c
+++ cadastro-cartas-Hilarionn-main/desafioxadrez.c
@@ -14,27 +14,29 @@ for (direita_torre = 0; direita_torre < 5; direita_torre ++ ) {
 printf ("A torre concluiu o movimento, totalizando %d casas para a direita!\n", direita_torre);
 
 
-int cima_bispo, direita_bispo, diagonal_bispo1qdt;
-
-
-diagonal_bispo1qdt = (cima_bispo + direita_bispo) / 2;
-cima_bispo = 5;
-direita_bispo = 5;
-
-for (diagonal_bispo1qdt = 0; diagonal_bispo1qdt < 5; diagonal_bispo1qdt ++ ) {
-    if (diagonal_bispo1qdt == 0) {
-    printf("O bispo uma casa para a diagonal!\n");
-    }
-    else { printf("O bispo andou mais uma casa para a diagonal!\n");}
-
-
+int casas_bispo, movimento_bispo;
+casas_bispo = 5;
+movimento_bispo = 0;
+while (movimento_bispo < casas_bispo) {
+         movimento_bispo ++;
+    printf("O bispo andou 1 casa na diagonal (para cima e uma para a direita!)\n");
 
 }
-
-printf ("O bispo concluiu o movimento, totalizando %d casas para cima e %d para a direita! (%d casas na diagonal)!\n", cima_bispo, direita_bispo, diagonal_bispo1qdt);
-
+printf("O bispo concluiu seu movimento andando %d casas na diagonal!\n", movimento_bispo);
 
 
 
+    int casas_rainha, movimento_rainha;
+    casas_rainha = 8;
+    movimento_rainha = 0;
+    do {
+    printf("A rainha andou 1 casa para a esquerda!\n");
+movimento_rainha ++;}
+ while 
+(casas_rainha > movimento_rainha);
+
+printf("A rainha concluiu seu movimento andando %d casas para a esquerda!\n", movimento_rainha);
     return 0;
 }
+
+
