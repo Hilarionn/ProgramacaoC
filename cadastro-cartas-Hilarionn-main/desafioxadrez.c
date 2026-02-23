5c76dbe28540439a6cd55320165c7231b993f1d4
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-23 12:33:15 +0000

    bispo

--- cadastro-cartas-Hilarionn-main/desafioxadrez.c
+++ cadastro-cartas-Hilarionn-main/desafioxadrez.c
@@ -14,14 +14,33 @@ for (direita_torre = 0; direita_torre < 5; direita_torre ++ ) {
 printf ("A torre concluiu o movimento, totalizando %d casas para a direita!\n", direita_torre);
 
 
-int cima_bispo, direita_bispo;
-for (cima_bispo = 0; cima_bispo < 5; cima_bispo ++ ) {
-    if (cima_bispo == 0) {
-    printf("O bispo uma casa para cima!\n");
+int cima_bispo, direita_bispo, diagonal_bispo1qdt;
+
+
+diagonal_bispo1qdt = (cima_bispo + direita_bispo) / 2;
+cima_bispo = 5;
+direita_bispo = 5;
+
+for (diagonal_bispo1qdt = 0; diagonal_bispo1qdt < 5; diagonal_bispo1qdt ++ ) {
+    if (diagonal_bispo1qdt == 0) {
+    printf("O bispo uma casa para a diagonal!\n");
     }
-    else { printf("O bispo andou mais uma casa para cima!\n");}
+    else { printf("O bispo andou mais uma casa para a diagonal!\n");}
+
+
+
 }
-printf ("O bispo concluiu o movimento, totalizando %d casas para cima e %d para a direita!\n", cima_bispo, direita_bispo);
+
+  /*  for (direita_bispo = 0; direita_bispo < 5; direita_bispo ++ ) {
+    if (direita_bispo == 0) {
+    printf("O bispo uma casa para a direita!\n");
+    }
+    else { printf("O bispo andou mais uma casa para a direita!\n");}
+
+
+}
+    */
+printf ("O bispo concluiu o movimento, totalizando %d casas para cima e %d para a direita! (%d casas na diagonal)!\n", cima_bispo, direita_bispo, diagonal_bispo1qdt);
 
 
 
