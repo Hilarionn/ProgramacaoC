1f797534e5af21f6db4c70a0b4b5c638e86bc0ef
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-23 23:39:22 +0000

    tabuada

--- cadastro-cartas-Hilarionn-main/desafioxadrez.c
+++ cadastro-cartas-Hilarionn-main/desafioxadrez.c
@@ -59,7 +59,17 @@ int segundos;
         }
     }
  
+
     printf("Fogos de artifício!\n");
+
+
+        for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+
 
     return 0;
 }
