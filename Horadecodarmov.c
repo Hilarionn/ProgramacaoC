b61100c98a30cfa1658d82e069db95fa66b0fbd6
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-23 11:21:33 +0000

    atualização

--- Horadecodarmov.c
+++ Horadecodarmov.c
@@ -47,7 +47,7 @@ int numero, i;
 
 printf("Digite um número para calcularmos a tabuada:\n");
 scanf("%d,", &numero);
-for (i = 0; i < 18881; i++)
+for (i = 0; i < 10; i++)
 {    printf("%d x %d = %d\n", i, numero, i * numero);
 }
     return 0;
