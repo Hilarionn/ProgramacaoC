fffbe023d46cb14aa9606a2d55268471efb268b9
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-02-28 14:20:21 +0000

    xadrez

--- desafio_mestre_xadrez.c
+++ desafio_mestre_xadrez.c
@@ -1,16 +1,74 @@
 #include <stdio.h>
 
-void MoverTorre(int casas){
-    if (casas < 5){
+/* TORRE - Recursiva */
+void MoverTorre(int atual, int limite){
+    if (atual < limite){
         printf("Direita\n");
-        MoverTorre(casas + 1);
+        MoverTorre(atual + 1, limite);
+    }
+}
+
+/* RAINHA - Recursiva */
+void MoverRainha(int atual, int limite){
+    if (atual < limite){
+        printf("Cima\n");
+        MoverRainha(atual + 1, limite);
+    }
+}
+
+/* BISPO - Recursivo + loops aninhados */
+void MoverBispo(int atual, int limite){
+    if (atual < limite){
+
+        for (int v = 0; v < 1; v++){
+            for (int h = 0; h < 1; h++){
+                printf("Cima\n");
+                printf("Direita\n");
+            }
+        }
+
+        MoverBispo(atual + 1, limite);
+    }
+}
+
+/* CAVALO - loops complexos */
+void MoverCavalo(){
+
+    int cima = 0;
+    int direita = 0;
+
+    while (cima < 2){
+
+        cima++;
+        printf("Cima\n");
+
+        if (cima == 2){
+
+            while (direita < 1){
+                direita++;
+                printf("Direita\n");
+            }
+        }
     }
 }
 
 int main(){
 
-MoverTorre(0);
+    int casas_torre = 5;
+    int casas_rainha = 3;
+    int casas_bispo = 2;
 
+    MoverTorre(0, casas_torre);
+    printf("\n");
+
+    MoverRainha(0, casas_rainha);
+    printf("\n");
+
+    MoverBispo(0, casas_bispo);
+    printf("\n");
+
+    MoverCavalo();
+    printf("\n");
 
     return 0;
 }
