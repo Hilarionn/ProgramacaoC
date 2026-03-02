373bbe43a79674ea07145af8eecbb21ded898c5d
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-03-02 22:59:23 +0000

    matriz

--- Matrizes.c
+++ Matrizes.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
  
 int main() {
-    int matriz[3][3] = {
+    int matriz[3][3] = {  
+        
+        //Sua matriz não pode mudar no decorrer da criação, nossa matriz assim como os vetores tem tamanho fixo e o acesso
+        //Sempre será pelo Índice. Os dois valores serão apontados dentro da matriz, como se fossem coordenadas X e Y. 
+        //Todos os elementos devem possuir o mesmo tipo (Caracteres, decimais, inteiros...)
         
         {1,2,3},
         {4,5,6},
