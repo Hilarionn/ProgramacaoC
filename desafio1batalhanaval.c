b83c9fc1feacbf194f97d9aaa6bf7478d613eda0
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-03-03 22:20:02 +0000

    saquei

--- desafio1batalhanaval.c
+++ desafio1batalhanaval.c
@@ -21,7 +21,15 @@ for (int i = 0; i < 3; i++) {
 
     tabuleiro[0][0+i] = 3;
 }
-
+//===========================================================================================================================================
+/* Neste loop, o índice 'i' é usado para percorrer as colunas na mesma linha.
+Como a matriz começa em 0, ao definir 'i' de 0 até 2, o código preenche exatamente três colunas consecutivas na mesma linha.
+ Ou seja, quando 'i' é 0, colocamos o valor na coluna 0; quando 'i' é 1, na coluna 1; e quando 'i' é 2, na coluna 2.
+ Isso cria um navio horizontal de tamanho 3, ocupando três casas consecutivas.
+ Esse padrão funciona porque o loop garante que 'i' percorra as colunas, e assim a gente preenche exatamente as três posições do navio.
+ Importante notar que, apesar de o loop estar fixo, o uso do 'i' permite que o código seja escalável.
+ bastando mudar a linha ou a coluna inicial para posicionar outros navios ou padrões.  */
+//===========================================================================================================================================
 
 
 
