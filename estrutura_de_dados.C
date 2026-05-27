5f30c8e47e1783196cb0a5538eaec171a64ce9e7
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-05-27 23:37:11 +0000

    aaaa

--- estrutura_de_dados.C
+++ estrutura_de_dados.C
@@ -13,12 +13,15 @@ int main () {
 //Criando e inicializando uma variável do tipo aluno
 
 Aluno aluno1 = {"João", 20, 8.5};
+Aluno aluno2 = {"Marcelo", 2002, 8.77};
 
 //Acessando os dados como operador
 
 printf("Aluno: %s\n", aluno1.nome);
 printf("Idade: %d\n", aluno1.idade);
 printf("Média: %.2f\n", aluno1.media);
+
+printf("Aluno 2 tem: %s, %d, %.2f\n", aluno2.nome, aluno2.idade, aluno2.media);
 
 
 
