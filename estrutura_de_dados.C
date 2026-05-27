fb767df962958e9f19bf003293f32e2c9e4a2891
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-05-27 23:22:25 +0000

    teste

---
+++ estrutura_de_dados.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+    //Definindo a struct para Aluno
+typedef struct{
+    char nome[60];
+    int idade;
+    float media;
+}  Aluno;
+
+
+int main () {
+//Criando e inicializando uma variável do tipo aluno
+
+Aluno aluno1 = {"João", 20, 8.5};
+
+//Acessando os dados como operador
+
+
+
+
+    return 0;
+}
