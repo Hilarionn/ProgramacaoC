4dfcc8bb3dfca49b901274e3e1a65fae46c88a68
Hilarionn <155191160+Hilarionn@users.noreply.github.com>
2026-05-30 14:03:51 +0000

    VSCODE

---
+++ war.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+// -------- Inclusão das constantes globais
+#define MAX_PAISES 5
+
+// -------- Função para limpar o buffer de entrada
+void LimparBufferEntrada (){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+
+// ------------- Definindo as Structs
+
+struct PAIS{
+    char nome[50];
+    char cor[10];
+    int tropas;
+}
+
+// ------------- Função principal
+
+int main (){
+
+
+
+
+
+
+
+
+    return 0;
+}
